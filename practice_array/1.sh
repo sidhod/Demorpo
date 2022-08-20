1d3103cf750a1f5cfc050a560ec5be79762ba2d1
unknown <sidhodhank2018@gmail.com>
2022-08-20 23:02:32 +0530

    Practice Problems

--- practice_array/1.sh
+++ practice_array/1.sh
@@ -1,21 +1,23 @@
-s1=999999 
-s2=999999 
+s1=999999
+s2=999999
 l1=99
 l2=99
-for i in $(seq 1 10) 
-do 
-	random=$(( $RANDOM % 900 + 100 )) 
-	printf "$random " 
-	if [ $s1 -gt $random ] 
-	then 
-		s2=$s1 
-		s1=$random
-	elif [ \( $s2 -gt $random \) -a \( $s1 -ne $random \) ] 
-	then 
-		s2=$random
-	fi
+declare -a array=();
+for i in $(seq 1 10)
+do
+        random=$(( $RANDOM % 900 + 100 ))
+        printf "$random "
+        array+=($random)
+        if (( $s1 > $random ))
+        then
+                s2=$s1
+                s1=$random
+        elif [ \( $s2 -gt $random \) -a \( $s1 -ne $random \) ]
+        then
+                s2=$random
+        fi
 
-	if [ $l1 -lt $random ]
+        if [ $l1 -lt $random ]
         then
                 l2=$l1
                 l1=$random
@@ -23,10 +25,12 @@ do
         then
                 l1=$random
         fi
- 
+
 done
-echo " "  
-echo "Smallest $s1" 
-echo "Second smallest $s2" 
+echo " "
+echo "Smallest $s1"
+echo "Second smallest $s2"
 echo "largest $l1"
 echo "Second largest $l2"
+echo "stored array: ${array[@]}"
+
