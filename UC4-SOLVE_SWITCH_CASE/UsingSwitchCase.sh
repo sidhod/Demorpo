385d930b62c7330ca4458a0b041ee03da15b4efc
unknown <sidhodhank2018@gmail.com>
2022-08-21 12:38:36 +0530

    Day9:UC4-SOLVE USING SWITCH CASE

---
+++ UC4-SOLVE_SWITCH_CASE/UsingSwitchCase.sh
@@ -0,0 +1,71 @@
+printf "
+    ##     ## ###### ##     ###### ###### ###   ### ######
+ *  ##     ## ##     ##     ##     ##  ## ## # # ## ##      *
+*** ##  #  ## #####  ##     ##     ##  ## ##  #  ## #####  ***
+ *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *
+    ###   ### ###### ###### ###### ###### ##     ## ######
+"
+echo "           * EMPLOYEE WAGE COMPUTATION *          "
+
+printf "            Select Your choice:                                       "
+printf "	
+		1.Attendance of Employee
+		2.Daily Employee Wage
+		3.Part Time Employee wage	"
+
+echo ""
+read -p "     Select Your choice: " sel
+case $sel in
+        1)
+		echo ""
+                 Attendance=$(( RANDOM % 2 + 1));
+		if [ $Attendance -eq 1 ]
+		then
+        	echo "Employee is Present";
+		else
+        	echo "Employee is Absent ";
+		fi
+		
+                ;;
+        2)
+		echo "";
+                echo "Wage per Hours is Rs 20";
+		wage_per_hours=20;
+		echo "Full Day Hour is 8";
+		hours=8;
+		Daily_wage=$(($wage_per_hours * $hours ));
+		echo "Daily Employee Wage : Rs $Daily_wage";
+                ;;
+        3)
+                echo "";
+		echo "Part time Hour is 8";
+		hour=8
+		echo "Wage per hour 20"
+		wph=20
+		part_time_wage=$(($hour*$wph))
+		echo "Part time employee wage: Rs $part_time_wage"
+
+                ;;
+        4)
+                echo "Four";
+                ;;
+        5)
+                echo "Five";
+                ;;
+        6)
+                echo "Six";
+                ;;
+        7)
+                echo "Seven";
+                ;;
+        8)
+                echo "Eight";
+                ;;
+        9)
+                echo "Nine";
+                ;;
+        *)
+                echo "Invalid choice";
+                ;;
+esac
+
